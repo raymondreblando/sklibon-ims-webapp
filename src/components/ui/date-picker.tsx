bc16b146def936e397e79094fcc840ff61152a96
Raymond <raymond.republica.reblando@gmail.com>
2025-08-27 22:06:09 +0800

    added ui primitives

---
+++ src/components/ui/date-picker.tsx
@@ -0,0 +1,59 @@
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import type { FieldValues, ControllerRenderProps } from "react-hook-form";
+
+import { cn } from "@/lib/utils";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { FormControl } from "@/components/ui/form";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+interface DatePickerProps<T extends FieldValues> {
+  field: ControllerRenderProps<T>;
+  size: "default" | "sm" | "lg";
+}
+
+export const DatePicker = <T extends FieldValues>({
+  field,
+  size = "default",
+}: DatePickerProps<T>) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <FormControl>
+          <Button
+            variant="outline"
+            size={size}
+            className={cn(
+              "hover:text-foreground w-full pl-3 text-left font-normal hover:bg-transparent",
+              !field.value && "text-muted-foreground",
+            )}
+          >
+            {field.value ? (
+              format(field.value, "PPP")
+            ) : (
+              <span>Pick a date</span>
+            )}
+            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+          </Button>
+        </FormControl>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0" align="start">
+        <Calendar
+          mode="single"
+          selected={field.value}
+          onSelect={field.onChange}
+          disabled={(date) =>
+            date > new Date() || date < new Date("1900-01-01")
+          }
+          captionLayout="dropdown"
+        />
+      </PopoverContent>
+    </Popover>
+  );
+};
