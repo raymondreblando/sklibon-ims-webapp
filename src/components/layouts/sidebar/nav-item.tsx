0bb6107da9b0f981bed00e2e501ea0b169259343
Raymond <raymond.republica.reblando@gmail.com>
2025-08-30 09:14:15 +0800

    added tooltip for sidebar item

--- src/components/layouts/sidebar/nav-item.tsx
+++ src/components/layouts/sidebar/nav-item.tsx
@@ -1,11 +1,19 @@
 import type { SidebarItemProps } from "@/types";
-import { SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import {
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
 import { Link } from "@tanstack/react-router";
 
 export const NavItem = ({ item }: { item: SidebarItemProps }) => {
+  const { open } = useSidebar();
   return (
     <SidebarMenuItem key={item.title}>
-      <SidebarMenuButton asChild>
+      <SidebarMenuButton
+        tooltip={{ children: item.title, hidden: !!open }}
+        asChild
+      >
         <Link to={item.url}>
           {item.icon && <item.icon className="h-12 w-12" />}
           <span className="text-base font-medium">{item.title}</span>
