d59f7f876d5019b7f71abc1449f349de690b4fad
Raymond <raymond.republica.reblando@gmail.com>
2025-08-30 12:52:33 +0800

    added form button component and refactor login and register form

---
+++ src/components/forms/fields/form-button.tsx
@@ -0,0 +1,34 @@
+import type { ComponentProps } from "react";
+import { Loader2Icon } from "lucide-react";
+import type { VariantProps } from "class-variance-authority";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils/utils";
+
+interface FormButtonProps
+  extends ComponentProps<"button">,
+    VariantProps<typeof buttonVariants> {
+  isSubmitting: boolean;
+  loaderClassname?: string;
+}
+
+export const FormButton = ({
+  children,
+  size = "default",
+  isSubmitting,
+  loaderClassname,
+  ...props
+}: FormButtonProps) => {
+  return (
+    <Button
+      size={size}
+      {...(isSubmitting && { disabled: true })}
+      {...props}
+      className={cn(isSubmitting && "cursor-progress")}
+    >
+      {isSubmitting && (
+        <Loader2Icon className={cn("animate-spin", loaderClassname)} />
+      )}
+      {children}
+    </Button>
+  );
+};
--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -2,6 +2,7 @@ export { FormAsyncSelect } from "./fields/form-async-select";
 export { FormInput } from "./fields/form-input";
 export { FormPasswordInput } from "./fields/form-password-input";
 export { FormSelect } from "./fields/form-select";
+export { FormButton } from "./fields/form-button";
 
 export { LoginForm } from "./login-form";
 export { RegisterForm } from "./register-form";
