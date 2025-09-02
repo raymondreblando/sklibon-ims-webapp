e1bf5db108c0b239d347f66d81807a26fc491481
Raymond <raymond.republica.reblando@gmail.com>
2025-09-02 21:13:29 +0800

    organize the inerface and type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,35 +1,12 @@
 import type { LucideIcon } from "lucide-react";
 import type { FieldValues, Path } from "react-hook-form";
 
-export type ApiResponse<T> = {
-  status: "success" | "error";
-  message: string;
-  data: T;
-};
-
 export interface FormField<T extends FieldValues> {
   name: Path<T>;
   label: string;
   placeholder?: string;
   className?: string;
 }
-
-export type Option = {
-  key: string;
-  value: string;
-};
-
-export type InputType =
-  | "text"
-  | "number"
-  | "email"
-  | "url"
-  | "tel"
-  | "search"
-  | "color"
-  | "file"
-  | "hidden"
-  | "range";
 
 export interface SidebarItemProps {
   title: string;
@@ -59,3 +36,26 @@ export interface ModalProps<TData = undefined> {
   onClose: () => void;
   data?: TData;
 }
+
+export type ApiResponse<T> = {
+  status: "success" | "error";
+  message: string;
+  data: T;
+};
+
+export type Option = {
+  key: string;
+  value: string;
+};
+
+export type InputType =
+  | "text"
+  | "number"
+  | "email"
+  | "url"
+  | "tel"
+  | "search"
+  | "color"
+  | "file"
+  | "hidden"
+  | "range";
