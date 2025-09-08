a1e61f8da4730d57d670f938428f6b4761ae147f
Raymond <raymond.republica.reblando@gmail.com>
2025-09-09 07:01:50 +0800

    added users api services

--- src/services/api/users.ts
+++ src/services/api/users.ts
@@ -1,16 +1,42 @@
 import { api } from "@/lib/axios";
-import type { UpdateUserField, UserProfileField } from "@/lib/schemas/user";
 import type { ApiResponse } from "@/types";
 import type { UserWithRelation } from "@/types/schema";
+
 import { API_ENDPOINTS } from "@/lib/constants/api-constants";
+import type { CreateUserField, UpdateUserField } from "@/lib/schemas/user";
+
+export const getUsers = async () => {
+  const { data: response } = await api.get<ApiResponse<UserWithRelation[]>>(
+    API_ENDPOINTS.USERS,
+  );
+
+  return response;
+};
+
+export const createUser = async (data: CreateUserField) => {
+  const { data: response } = await api.post<ApiResponse<UserWithRelation>>(
+    API_ENDPOINTS.USERS,
+    data,
+  );
+
+  return response;
+};
 
 export const updateUser = async (
   id: string | undefined,
-  data: UserProfileField | UpdateUserField,
+  data: UpdateUserField,
 ) => {
   const { data: response } = await api.put<ApiResponse<UserWithRelation>>(
     `${API_ENDPOINTS.USERS}/${id}`,
     data,
+  );
+
+  return response;
+};
+
+export const deleteUser = async (id: string) => {
+  const { data: response } = await api.delete<ApiResponse<UserWithRelation>>(
+    `${API_ENDPOINTS.USERS}/${id}`,
   );
 
   return response;
