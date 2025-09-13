ea9d9c5f756673bb55116189c3313bdaa8eae168
Raymond <raymond.republica.reblando@gmail.com>
2025-09-13 19:03:18 +0800

    refactor user-profile-card skeleton

--- src/components/skeletons/user-profile-card-skeleton.tsx
+++ src/components/skeletons/user-profile-card-skeleton.tsx
@@ -1,14 +1,10 @@
 import { Skeleton } from "@/components/ui/skeleton";
+import { ProfileCardUserSkeleton } from "./profile-card-user-skeleton";
 
 export const UserProfileCardSkeleton = () => {
   return (
     <div className="mx-auto flex max-w-[640px] flex-col gap-y-4 border-0 pt-20 shadow-none md:pt-32">
-      <div className="border-input relative flex flex-col items-center justify-center gap-y-2 rounded-md border pt-20 pb-8">
-        <Skeleton className="border-input absolute -top-20 left-1/2 min-h-36 min-w-36 -translate-x-1/2 rounded-full border-4" />
-        <Skeleton className="h-8 w-48 rounded-full" />
-        <Skeleton className="h-4 w-16 rounded-full" />
-        <Skeleton className="h-4 w-32 rounded-full" />
-      </div>
+      <ProfileCardUserSkeleton />
       <div className="flex flex-col gap-y-4 px-0">
         {Array.from({ length: 2 }).map((_, index) => (
           <div
