20d22ff56e0eee4422e6b732fcd5d435ef0b822d
Raymond <raymond.republica.reblando@gmail.com>
2025-09-21 13:30:53 +0800

    added the request table

--- src/routes/_main/requests/index.tsx
+++ src/routes/_main/requests/index.tsx
@@ -1,26 +1,38 @@
+import { useCallback, useEffect } from "react";
 import { createFileRoute } from "@tanstack/react-router";
+import type { RequestWithRelation } from "@/types/schema";
 
-import { ButtonLink } from "@/components/buttons";
-import { Searchbar } from "@/components/ui/searchbar";
+import { useModal } from "@/contexts/modal-context";
+import { useDeleteRequestMutation } from "@/hooks/mutations/use-request-mutations";
+
+import { useBreadcrumb } from "@/components/ui/breadcrumb";
+import { DeleteConfirmationDialog } from "@/components/modals";
+import { RequestTable } from "@/components/tables/request";
 
 export const Route = createFileRoute("/_main/requests/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return (
-    <>
-      <div className="border-input flex items-center justify-between gap-x-4 border-y bg-white px-6 py-4">
-        <Searchbar
-          inputProps={{
-            placeholder: "Search here...",
-            onInput: (event) => console.log(event.currentTarget.value),
-          }}
-        />
-        <div className="flex items-center space-x-4">
-          <ButtonLink to="/requests/add">Create Request</ButtonLink>
-        </div>
-      </div>
-    </>
+  const { setItems } = useBreadcrumb();
+  const deleteRequest = useDeleteRequestMutation();
+  const { show } = useModal();
+
+  useEffect(() => {
+    setItems([{ title: "Requests" }]);
+  }, [setItems]);
+
+  const onDelete = useCallback(
+    (request: RequestWithRelation) => {
+      show(
+        <DeleteConfirmationDialog
+          onConfirm={() => deleteRequest.mutate(request.id)}
+          isConfirming={deleteRequest.isPending}
+        />,
+      );
+    },
+    [deleteRequest, show],
   );
+
+  return <RequestTable onDelete={onDelete} />;
 }
