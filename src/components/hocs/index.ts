1fd890ac8e1bd927dd8fcada6bcb4b582799c0df
Raymond <raymond.republica.reblando@gmail.com>
2025-10-11 06:27:13 +0800

    added with-auth-guard component and applied guard for event viewing

--- src/components/hocs/index.ts
+++ src/components/hocs/index.ts
@@ -1,3 +1,4 @@
 export { EmptyStateWrapper } from "./empty-state-wrapper";
 export { QueryStatusWrapper } from "./query-status-wrapper";
+export { WithAuthGuard } from "./with-auth-guard";
 export { WithRoleGuard } from "./with-role-guard";
