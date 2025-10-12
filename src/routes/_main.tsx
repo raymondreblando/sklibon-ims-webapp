12af94ee51f1ba46985e4f62abe7a9ebdcdda2d3
Raymond <raymond.republica.reblando@gmail.com>
2025-10-12 12:30:31 +0800

    feat: implemented realtime notifications

--- src/routes/_main.tsx
+++ src/routes/_main.tsx
@@ -1,12 +1,17 @@
+import { useQueryClient } from "@tanstack/react-query";
+import { useEcho, useEchoPublic } from "@laravel/echo-react";
 import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 
+import { getAuthUser } from "@/lib/utils/auth";
 import { isAuthenticated } from "@/lib/utils/auth";
+import { QUERY_KEYS } from "@/lib/constants/api-constants";
 import "@/lib/utils/echo";
 
 import { AppSidebar } from "@/components/layouts/sidebar/app-sidebar";
 import { BreadcrumbProvider } from "@/components/ui/breadcrumb";
 import { SecondaryNav } from "@/components/layouts/secondary-nav";
 import { SidebarProvider } from "@/components/ui/sidebar";
+
 
 export const Route = createFileRoute("/_main")({
   component: RouteComponent,
@@ -23,6 +28,28 @@ export const Route = createFileRoute("/_main")({
 });
 
 function RouteComponent() {
+  const queryClient = useQueryClient();
+  const user = getAuthUser();
+
+  useEcho(`notification.user.${user?.id}`, ".notification.created", () => {
+    queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.NOTIFICATIONS] });
+    queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.REQUESTS] });
+  });
+
+  useEcho(
+    `notification.barangay.${user?.info.barangay.id}`,
+    ".notification.created",
+    () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.NOTIFICATIONS] });
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.REQUESTS] });
+    },
+  );
+
+  useEchoPublic("notification.announcements", ".notification.created", () => {
+    queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.NOTIFICATIONS] });
+    queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.EVENTS] });
+  });
+
   return (
     <SidebarProvider>
       <AppSidebar />
